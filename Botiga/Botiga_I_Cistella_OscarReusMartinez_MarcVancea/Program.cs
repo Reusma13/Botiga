5e8e0de1aa4c441be4972767b28b330b6635dfd6
Reusma13 <a2308764@institutmontilivi.cat>
2024-02-12 11:00:43 +0100

    Inici projecte

---
+++ Botiga/Botiga_I_Cistella_OscarReusMartinez_MarcVancea/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Botiga_I_Cistella_OscarReusMartinez_MarcVancea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
